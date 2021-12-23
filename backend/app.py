200a939784d7868b26bf54b030cfe9e333450685
Vera Wang <verawang@Veras-MacBook-Pro.local>
2021-12-23 13:46:32 -0800

    i'm rekt

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
+# Import the required packages
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
+ma = Marshmallow()
+cors = CORS()
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def create_app():
+    """Application-factory pattern"""
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize extensions
+    # To use the application instances above, instantiate with an application:
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+    cors.init_app(app)
+
+    return app
