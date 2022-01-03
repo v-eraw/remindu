ac7b243f307730d2740f2fd535f5b6b7199c19b3
Vera Wang <veraw@berkeley.edu>
2022-01-03 07:47:14 -0800

    Added user Id

--- backend/app/models/reminder_history_log.py
+++ backend/app/models/reminder_history_log.py
@@ -6,11 +6,13 @@ class ReminderHistoryLog(db.Model):
 
     log_id = db.Column(db.Integer(), primary_key=True)
     reminder_id = db.Column(db.String, primary_key=False)
+    user_id = db.Columnt(db.String, primary_key=False)
     datetime = db.Column(db.datetime, nullable=False)
 
-    def __init__(self, log_id, reminder_id, datetime):
+    def __init__(self, log_id, reminder_id, user_id, datetime):
         self.log_id = log_id
         self.reminder_id = reminder_id
+        self.user_id = user_id
         self.datetime = datetime
 
     def __repr__(self):
