e9f075d2ce9bfc714e75ad27e3b8dee8ad9c930d
Vera Wang <veraw@berkeley.edu>
2022-01-06 19:06:49 -0800

    new boilerplate

--- client/components/environment/Main/index.js
+++
@@ -1,5 +0,0 @@
-import { withRouter } from 'react-router-dom';
-
-import Main from './Main';
-
-export default withRouter(Main);
--- client/utils/strings.js
+++
@@ -1,3 +0,0 @@
-import R from 'ramda';
-
-export const capitalize = string => R.concat(R.toUpper(R.head(string)), R.tail(string));
