61b58565c57ef071ede92803fe2a434992353aaf
Vera Wang <veraw@berkeley.edu>
2022-02-14 13:02:30 -0800

    update

--- routes/reminders.js
+++ routes/reminders.js
@@ -1,13 +1,53 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var router = express.Router();
 
-let reminders = []
+reminders = [ 
+  {
+    id: "1",
+    description: "wash your dishes",
+    datetime: Date.now(),
+    frequency: "daily",
+  },
+  {
+    id: "2",
+    description: "call your mom",
+    datetime: Date.now(),
+    frequency: "daily",
+  },
+  {
+    id: "3",
+    description: "book a ticket to france",
+    datetime: Date.now(),
+    frequency: "onetime",
+  },
+]
 
-app.get('/api/reminders', (request, response) => {
-  response.json(notes)
-})
+/* GET all reminders listing. */
+router.get('/', function(req, res, next) {
+  console.log("getting reminders")
+  res.send(reminders);
+});
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+/* GET the details for a reminders listing. */
+router.get('/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* UPDATE reminders listing. */
+router.put('/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* POST reminders listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* DELETE reminders listing. */
+router.delete('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+
+module.exports = router;
