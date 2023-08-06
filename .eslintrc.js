6c1f72bb5330ad50649d6fdb687b4bae25bbf020
Vera Wang <veraw@berkeley.edu>
2023-08-05 21:47:03 -0700

    fix babel eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
-    parser: 'babel-eslint',
-    plugins: ['react', 'prettier'],
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-    rules: {
-      'prettier/prettier': 'error',
-      // Add more rules or overrides as needed
+  parser: '@babel/eslint-parser',
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+  },
+  extends: [
+    'next',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    // Add more rules or overrides as needed
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
-  
+  },
+};
